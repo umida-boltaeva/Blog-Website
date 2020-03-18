16f6c281bcecc9dfa451aa186e1e10345d2bd0d1
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-03-18 21:04:24 +1100

    Give the blog a Database. Now I keep my posts in MongoDB! Yaaay!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,15 +12,28 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const globalPost = [];
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://admin-umida:ndTq&4+x$qVYg,A@cluster0-mggld.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
 app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent, posts: globalPost});  
+  Post.find({}, function(err, globalPost){
+    res.render("home", {
+        startingContent: homeStartingContent,
+        posts: globalPost
+    });
+});  
 });
 
 app.get("/about", function(req, res){
@@ -34,31 +48,27 @@ app.get("/compose", function(req, res){
   res.render("compose");
 });
 
+
+
 app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
+  const post = new Post({
+    title: req.body. postTitle,
     content: req.body.postBody
-  };
-
-  globalPost.push(post);
-  res.redirect("/");
-});
-
-app.get("/posts/:postName", function(req, res){
-  const requiredTitle = _.lowerCase(req.params.postName);
-
-  globalPost.forEach(post => {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requiredTitle) {
-      res.render("post", {title: post.title, content: post.content});
-      
-    }
   });
+  
+  post.save();
+ 
+  res.redirect("/");
+ 
+  }); 
+
+app.get("/posts/:postId", function(req, res){
+  const requiredPostId = req.params.postId;
+
+Post.findOne({_id: requiredPostId}, function(err, post){
+  res.render("post", {title: post.title, content: post.content});
 });
-
-
-
+});
 
 
 
